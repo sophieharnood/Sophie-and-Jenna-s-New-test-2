3a28162e6699b5c8a5d41c6868cfd546b2a38e52
Sophie <sophiaharnood@gmail.com>
2023-08-01 11:23:21 -0400

    Initial Commit

---
+++ Sophie and Jenna's New test 2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sophie and Jenna's New test 2
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sophie and Jenna's New test 2/Sophie_and_Jenna_s_New_test_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sophie_and_Jenna_s_New_test_2App.swift
+//  Sophie and Jenna's New test 2
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Sophie_and_Jenna_s_New_test_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
